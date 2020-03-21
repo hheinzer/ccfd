043da762ef1647229789bc986d9c2979d87ca2d7
Heinz Heinrich Heinzer <heinrich.heinzer@gmail.com>
2020-03-21 11:45:13 +0100

    finalizing output

--- src/main.c
+++ src/main.c
@@ -14,27 +14,37 @@
 int main(int argc, char *argv[])
 {
 	/* greeting */
-	printf("|===========================================================|\n");
-	printf("|                           C C F D                         |\n");
-	printf("|===========================================================|\n");
+	printf(" =========================================================== \n");
+	printf("                            C C F D                          \n");
+	printf(" =========================================================== \n");
+	printf("   Solution of the two-dimensional Euler and Navier-Stokes   \n");
+	printf("     equations using an unstructured finite volume solver    \n");
+	printf("                                                             \n");
+	printf("   This program may be used under the conditions of the GPL  \n");
+	printf("                                                             \n");
+	printf("  Created at the Institute of Aerodynamics and Gas Dynamics  \n");
+	printf("           at the University of Stuttgart, Germany           \n");
+	printf("               http://www.iag.uni-stuttgart.de               \n");
+	printf("                                                             \n");
+	printf("           Recreated in C by Heinz Heinrich Heinzer          \n");
+	printf(" =========================================================== \n\n");
 
 
 	/* read in parameter file */
 	char *iniFileName = "../calc/sod.ini";
 	fillCmds(iniFileName);
-	char *aStr = getStr("filename", NULL);
-
-	/*
-	 * test readInTools
-	 *
-	char *aStr = getStr("filename", NULL);
-	int aCount = countKeys("meshbctype", 3);
-	printf("%i\n", aCount);
-	int anInt = getInt("meshbctype", "440");
-	double aDbl = getDbl("tEnd", "0.334");
-	bool aBool = getBool("exactsolution", "F");
-	int *anIntArray = getIntArray("nBCsegments", 4, "3,4,2,1");
-	double *anDblArray = getDblArray("xmax", 2, "3.2,1.9");
-	ignoredCmds();
-	*/
 }
+
+/*
+ * test readInTools
+ *
+char *aStr = getStr("filename", NULL);
+int aCount = countKeys("meshbctype", 3);
+printf("%i\n", aCount);
+int anInt = getInt("meshbctype", "440");
+double aDbl = getDbl("tEnd", "0.334");
+bool aBool = getBool("exactsolution", "F");
+int *anIntArray = getIntArray("nBCsegments", 4, "3,4,2,1");
+double *anDblArray = getDblArray("xmax", 2, "3.2,1.9");
+ignoredCmds();
+*/
