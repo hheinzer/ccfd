d452ca2685a1a115b30f77d3c6284fd26f02e42e
Heinz Heinrich Heinzer <heinrich.heinzer@gmail.com>
2020-03-28 10:20:22 +0100

    added reconstruction.{c,h}

---
+++ src/reconstruction.c
@@ -0,0 +1,13 @@
+/*
+ * reconstruction.c
+ *
+ * Created: Sat 28 Mar 2020 10:17:02 AM CET
+ * Author : hhh
+ */
+
+#include "reconstruction.h"
+
+/* extern variables */
+int limiter;
+double venk_k;
+int nVarGrad;
---
+++ src/reconstruction.h
@@ -0,0 +1,15 @@
+/*
+ * reconstruction.h
+ *
+ * Created: Sat 28 Mar 2020 10:16:16 AM CET
+ * Author : hhh
+ */
+
+#ifndef RECONSTRUCTION_H
+#define RECONSTRUCTION_H
+
+extern int limiter;
+extern double venk_k;
+extern int nVarGrad;
+
+#endif
